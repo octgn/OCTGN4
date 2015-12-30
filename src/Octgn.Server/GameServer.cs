b57e4f6ab8934093a18d3777f16b0f1c239b10d8
Kelly Elton <its.the.doc@gmail.com>
2015-12-29 21:32:35 -0500

    Start of some socket shits

--- src/Octgn.Server/GameServer.cs
+++ src/Octgn.Server/GameServer.cs
@@ -1,6 +1,69 @@
-﻿namespace Octgn.Server
+﻿using System;
+using System.Net.Sockets;
+
+namespace Octgn.Server
 {
-    public class GameServer
+    public class GameServer : IDisposable
     {
+        private GameServerListener _listener;
+        public GameServer(int port)
+        {
+            _listener = new GameServerListener(port, OnSocket);
+        }
+
+        private void OnSocket(GameServerSocket sock)
+        {
+
+        }
+
+        public void Dispose()
+        {
+            _listener.Dispose();
+        }
+    }
+
+    internal class GameServerListener : IDisposable
+    {
+        private bool _disposed;
+        private TcpListener _listener;
+        private Action<GameServerSocket> _onSocket;
+        public GameServerListener(int port, Action<GameServerSocket> onSocket)
+        {
+            _listener = new TcpListener(System.Net.IPAddress.Any, port);
+            _onSocket = onSocket;
+            Run();
+        }
+
+        private async void Run()
+        {
+            var socket = await _listener.AcceptSocketAsync();
+            if(socket != null)
+            {
+                // Handle the socket
+                _onSocket(new GameServerSocket(socket));
+            }
+            lock (this)
+            {
+                if (_disposed) return;
+                Run();
+            }
+        }
+        public void Dispose()
+        {
+            lock (this)
+            {
+                if (_disposed) return;
+                _disposed = true;
+            }
+        }
+    }
+
+    internal class GameServerSocket
+    {
+        private Socket _socket;
+        public GameServerSocket(Socket sock)
+        {
+            _socket = sock;
+        }
     }
 }
