3becf912cce45d24a8073348ff32dd54697a241a
Kelly Elton <its.the.doc@gmail.com>
2016-01-14 18:10:47 -0500

    asdf

--- src/Octgn.Test/Shared/Networking/PacketTests.cs
+++ src/Octgn.Test/Shared/Networking/PacketTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using Octgn.Shared.Networking;
 using FakeItEasy;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 namespace Octgn.Test.Shared.Networking
 {
@@ -35,7 +36,7 @@ namespace Octgn.Test.Shared.Networking
         }
 
         [Test, Combinatorial]
-        public void Invoke_MultipleParameters([Values(null, "asdf")]string arg1, [Values(null, 2)]int? arg2, [Values(null, 1.4f)]float? arg3)
+        public void Invoke_MultipleParameters([Values(null, "asdf")]string arg1, [Values(null, 2)]int? arg2, [Values(null, 1.4f)]float? arg3, [Values(false, true)] bool doArg4, [Values(false, true)] bool doArg4AsJObject)
         {
             var packet = new NetworkProtocol.Packet()
             {
@@ -67,23 +68,35 @@ namespace Octgn.Test.Shared.Networking
                 };
                 pars.Add(p);
             }
+			var faketest = A.Fake<ITest1>();
+			var jobject = new JObject(faketest);
+			if(doArg4)
+			{
+				var p = new NetworkProtocol.MethodParameter()
+				{
+					Name = "d",
+					Value = (doArg4AsJObject ? (object)jobject : faketest)
+				};
+                pars.Add(p);
+			}
             packet.Parameters = pars.ToArray();
 
-            var faker = A.Fake<ITest1>();
-            packet.Invoke<ITest1>(faker);
+            packet.Invoke<ITest1>(faketest);
             var expectedA = arg1;
             var expectedB = arg2 == null ? default(int) : arg2.Value;
             var expectedC = arg3 == null ? default(float) : arg3.Value;
-            A.CallTo(() => faker.Method2(A<string>.That.IsEqualTo(expectedA)
+			var expectedD = doArg4 ? (doArg4AsJObject ? (object)jobject : faketest) : null;
+			A.CallTo(() => faketest.Method2(A<string>.That.IsEqualTo(expectedA)
                 , A<int>.That.IsEqualTo(expectedB)
-                , A<float>.That.IsEqualTo(expectedC))
+                , A<float>.That.IsEqualTo(expectedC)
+				, A<object>.That.IsEqualTo(expectedD))
             ).MustHaveHappened(Repeated.Exactly.Once);
         }
 
         public interface ITest1
         {
             void Method1();
-            void Method2(string a, int b, float c);
+            void Method2(string a, int b, float c, ITest1 d);
         }
     }
 }
