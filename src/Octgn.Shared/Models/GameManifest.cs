99b564d8c3eb3badbf6a61295f13c766ab19011e
Kelly Elton <its.the.doc@gmail.com>
2016-01-16 23:05:13 -0500

    Rename the layout to Stage, cause it has more meaning

--- src/Octgn.Shared/Models/GameManifest.cs
+++ src/Octgn.Shared/Models/GameManifest.cs
@@ -7,7 +7,7 @@ namespace Octgn.Shared.Models
 	{
 		public string Name { get; set; }
 		public Version Version { get; set; }
-		public string InitialLayout { get; set; }
+		public string InitialStage { get; set; }
 
 		public GameManifest()
 		{
