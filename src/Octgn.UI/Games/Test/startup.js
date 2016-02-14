20f337df060f2eb5ea2aeec93829287ea336001b
Kelly Elton <its.the.doc@gmail.com>
2016-02-14 00:51:04 -0500

    More option

--- src/Octgn.UI/Games/Test/startup.js
+++ src/Octgn.UI/Games/Test/startup.js
@@ -12,8 +12,8 @@ O.com.on('msg', function (obj) {
     O.com.broadcast('msg', obj);
 });
 
+O.state.chatLog = new ArrayStateObject();
 O.state.chatLog = O.statefull([]);
-
 // Array item added
 O.state.chatLog.push(1);
 O.state.chatLog.push(2);
@@ -22,7 +22,7 @@ delete O.state.chatLog[0]
 // Array item is changed
 O.state.chatLog[0] = 12
 
-O.state.someObj = O.statefull({});
+O.state.someObj = new StateObject();
 // Property is created
 O.state.someObj.taco = 12;
 O.state.someObj.taco2 = 12;
