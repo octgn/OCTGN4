f7bd05c3f44fc20a4c6c73b7d688043808e73c35
Kelly Elton <its.the.doc@gmail.com>
2016-06-30 22:40:24 -0400

    Fixed build error

--- src/Octgn.JS/Shared/Common.ts
+++ src/Octgn.JS/Shared/Common.ts
@@ -1,5 +1,4 @@
-﻿/// <reference path="./../typings/node/node.d.ts" />
-export type Callback = (sender: any, obj: any) => void;
+﻿export type Callback = (sender: any, obj: any) => void;
 
 export interface Dict<TVal> {
     [K: string]: TVal;
@@ -8,13 +7,3 @@ export interface Dict<TVal> {
 export class NotImplementedException {
 
 }
-
-export interface Object {
-    getName(): string;
-}
-
-Object.prototype.getName = function() {
-    var funcNameRegex = /function (.{1,})\(/;
-    var results = (funcNameRegex).exec((this).constructor.toString());
-    return (results && results.length > 1) ? results[1] : "";
-};
---
+++ src/Octgn.JS/Shared/Object.ts
@@ -0,0 +1,9 @@
+﻿interface Object {
+    getName(): string;
+}
+
+Object.prototype.getName = function() {
+    var funcNameRegex = /function (.{1,})\(/;
+    var results = (funcNameRegex).exec((this).constructor.toString());
+    return (results && results.length > 1) ? results[1] : "";
+};
