a2b2aeddabadfce5b0508982db5449e9fcf35921
Kelly Elton <its.the.doc@gmail.com>
2016-07-01 21:39:51 -0400

    Host and connect to host Move output folder to js subfolder

--- src/Octgn.JS/NodeHost/app.ts
+++ src/Octgn.JS/NodeHost/app.ts
@@ -1,13 +1,28 @@
 ﻿import * as nconf from 'nconf';
 import * as O from "./../Server/OServer";
+import * as WebSocket from 'ws';
 import {GameHost, HostConfig} from './GameHost';
 
+console.log("Loading Config");
 nconf.argv().env().file({ file: 'config.json' });
 
 var config = new HostConfig();
 config.Port = nconf.get("port");
 config.GameId = nconf.get("game");
 
+console.log("Creating host");
 var host = new GameHost(config);
+console.log("Starting host");
+host.Start();
 
 console.log('Server is running on port', host.Config.Port);
+
+var socket = new WebSocket('ws://localhost:' + config.Port);
+socket.onopen = function () {
+    console.log("Client connected");
+    socket.onmessage = function (mess) {
+        console.log("client: " + mess);
+    }
+
+    socket.send(JSON.stringify({ 'a': "b" }));
+}
